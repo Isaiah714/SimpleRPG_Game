8638b73f153ff2edb1b9c13775d27035d750ab4e
Isaiah714 <Ip714@csu.fullerton.edu>
2026-03-06 22:24:10 -0800

    Added smart pointers and an alias

--- include/gamepanel.hpp
+++ include/gamepanel.hpp
@@ -8,17 +8,31 @@
 #include <array>
 #include <chrono>
 #include <thread>
+#include <memory>
+
+#include "enemies.hpp"
 
 #define FRAME_SIZE 21
 
+using Inventory = std::array<std::pair<std::string, int>, 4>;
+//using EnemyAt = std::array<std::string, 5>;
+
 inline std::array<std::pair<std::string, int>, 4> playerInventory;
+inline std::array<std::string, 5> enemyAttributes;
+
+
+struct GameDialog
+{
+  std::string statement;
+  std::unique_ptr<Inventory> printInventory;
+  std::unique_ptr<Enemy> printEnemy;
+};
 
 void gamePanel();
-void animation( const std::string & filePath );
-void givePlayerSetOfItems();
+void animation( const std::string & filePath, GameDialog & dialog );
+void givePlayerSetOfItems( GameDialog & dialog );
 
 template<typename T>
-void spawnEnemy( const std::string & stage );
+void spawnEnemy( const std::string & stage, GameDialog & dialog );
 
 #endif
-
