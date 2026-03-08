33873540e8324ec855194b8f3268ffae47fbec29
Isaiah714 <Ip714@csu.fullerton.edu>
2026-03-07 23:26:54 -0800

    Changed class name to 'GameLoop'

---
+++ include/gameloop.hpp
@@ -0,0 +1,48 @@
+#ifndef __ASCII_ANIMATION_H__
+#define __ASCII_ANIMATION_H__
+
+#include <fstream>
+#include <sstream>
+#include <iostream>
+#include <string>
+#include <vector>
+#include <chrono>
+#include <thread>
+
+#include "gamepanel.hpp"
+
+#define FRAME_SIZE 21
+
+class FrameContainer
+{
+  public:
+  FrameContainer();
+
+  void setFrame( const std::string & collectedFrame );
+
+  std::string frame;
+};
+
+class GameLoop
+{
+  public:
+  GameLoop();
+  GameLoop( const std::string & filePath );
+
+  GameDialog * getDialog();
+  void setDialog( GameDialog & dialog );
+
+  friend std::ostream & operator<<( std::ostream & stream, GameLoop & frameBuffer);
+
+  private:
+  const std::string filePath__;
+  std::string frameBuffer__;
+  std::vector<FrameContainer> allFrames__;
+  GameDialog * dialog__;
+
+  const std::string printGameDialog() &;  
+
+  void readFile();
+};
+
+#endif
