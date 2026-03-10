33a26fec7b7ba5aa31b2a5756140c6472984d750
Isaiah714 <Ip714@csu.fullerton.edu>
2026-03-10 11:16:00 -0700

    Renamed folder

--- src/main.cpp
+++ src/main.cpp
@@ -5,3 +5,18 @@ int main()
   gamePanel();
   return 0;
 }
+
+#include <ncurses.h>
+
+// Testing with ncurses
+/*int main()
+{
+  GameDialog def;
+  initscr();
+  printw("Hello World");
+  animation("../ASCII_Frames/Menu/Title.txt", def);
+  refresh();
+  getch();
+  endwin();
+  return 0;
+}*/
